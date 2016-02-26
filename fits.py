94ad120f0823a469f24d5b728fde5b0537994e01
Kaylan Burleigh <kaylanb@berkeley.edu>
2016-02-26 17:53:56 -0300

    getdata() returns dict of numpy arrays and list of correctly ordered keys for making nicely ordered postgres table

--- fits.py
+++ fits.py
@@ -3,8 +3,14 @@ from astropy.io import fits
 from astrometry.util.fits import fits_table
 
 def getdata(hdulist,hdu):
-	'''returns tuple: data array,keys'''
-	return np.asarray(hdulist[hdu].data), hdulist[hdu].columns.names
+	'''returns tuple: data,keys
+	data: dict of numpy arrays
+	keys is correctly ordered list for data.keys()'''
+	data,ordered_keys= np.asarray(hdulist[hdu].data), hdulist[hdu].columns.names
+	d={}
+	for key in ordered_keys:
+		d[key]= data[key]
+	return d,ordered_keys
 
 def tractor_cat(fn):
 	'''uses Dustin's fits_table() function to read his tractor catalog then returns it as dictionary of np arrays
