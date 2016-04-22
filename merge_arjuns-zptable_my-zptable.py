019476f61f5189c3c7d9e6cfa508bbe1198971d0
Kaylan Burleigh <kburleigh@lbl.gov>
2016-04-22 10:40:41 -0700

    added code that correctly treats bok-ccds.fits provided by Ian like Arjun's zerpoints file, and makes final bok-ccds.fits file by combinging what I read from bok headers with Ian's zpts and other updated values

--- merge_arjuns-zptable_my-zptable.py
+++ merge_arjuns-zptable_my-zptable.py
@@ -1,6 +1,7 @@
 from astrometry.util.fits import fits_table,merge_tables
 import numpy as np
 from argparse import ArgumentParser
+import os
 
 parser = ArgumentParser(description="test")
 parser.add_argument("-arjuns",action="store",help='from decstat,mostat,bokstat',required=True)
@@ -13,16 +14,21 @@ m=fits_table(args.mine)
 comb=fits_table(args.mine) #copy of mine
 #fill zeros in my table for fields will fill in from arjun's table
 cols= ['ccdzpt','ccdzpta','ccdzptb','ccdphoff','ccdphrms','ccdskyrms','ccdraoff','ccddecoff','ccdtransp','ccdnstar','ccdnmatch','ccdnmatcha','ccdnmatchb','ccdmdncol'] #fwhm,arawgain
+if args.camera == 'bok':
+    cols+= ['arawgain','avsky','mjd_obs','expnum']
+    for rm_col in ['ccdphoff','ccdskyrms','ccdtransp']: cols.pop( cols.index(rm_col) ) #ians bok ccds.fits does not have these
 for col in cols: comb.set(col, np.zeros(m.get('ra').shape).astype(a.get(col).dtype) ) #junk for now, but with correct dtype
 #for each filename, get index of matching ccd number, fill in new info at that index
-for fn in a.get('filename'):
+for fn in m.get('image_filename'):
+    fn= os.path.basename(fn)
     for ccd in range(1,5):
-        i_arj_fn= a.get('filename') == fn
+        if args.camera == 'mosaic': i_arj_fn= a.get('filename') == fn
+        else: i_arj_fn= a.get('image_filename') == fn
         i_arj_ccd= a.get('ccdnum') == ccd
         i_arj= np.all((i_arj_fn,i_arj_ccd),axis=0)
         assert(np.any(i_arj))
         i_me_fn= m.get('image_filename') == args.camera+'/'+fn #arjuns fn is basname, my file name is camera/basename
-        i_me_ccd= m.get('ccdnum') == ccd
+        i_me_ccd= m.get('ccdnum').astype(int) == ccd
         i_me= np.all((i_me_fn,i_me_ccd),axis=0)
         assert(np.any(i_me))
         #fill in arjun's info
