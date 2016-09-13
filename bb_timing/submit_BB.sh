a589d10a7de0b24405ea43be8d1619b82aa16a7a
Kaylan Burleigh <kaylanb@berkeley.edu>
2016-09-13 13:59:33 -0700

    replaced submit scripts with Chris D's updated version for better handling of multi node

--- bb_timing/submit_BB.sh
+++ bb_timing/submit_BB.sh
@@ -1,36 +1,28 @@
 #!/bin/bash -l
 
 #SBATCH -p debug
-#SBATCH -N 64
-#SBATCH -t 00:30:00
+#SBATCH -N 2
+#SBATCH -t 00:05:00
 #SBATCH -J bb_multi
-#SBATCH -o bb_multi.o%j
+#SBATCH -o output.%j
+#SBATCH -e error.%j
+#SBATCH -d singleton
 #SBATCH --mail-user=kburleigh@lbl.gov
 #SBATCH --mail-type=END,FAIL
 #DW persistentdw name=tractorSmall
 #DW stage_out source=$DW_PERSISTENT_STRIPED_tractorSmall/bb_multi destination=/global/cscratch1/sd/kaylanb/BB/bb_multi type=directory
 
+set -x
+LAUNCH="./sync_launch.sh"
+export CONTROL_FILE="$SCRATCH/control_file_${SLURM_JOBID}.txt"
+
 export outdir=$DW_PERSISTENT_STRIPED_tractorSmall/bb_multi
 mkdir ${outdir}
-#export cori_outdir=/global/cscratch1/sd/kaylanb/BB/myout
-#rm -r ${cori_outdir}/*
 
 ncores=32
-#export savedir=$SLURM_SUBMIT_DIR/testipm_${ncores}
-#mkdir ${savedir}
 
 export LEGACY_SURVEY_DIR=$DW_PERSISTENT_STRIPED_tractorSmall/dr3
 export DUST_DIR=$DW_PERSISTENT_STRIPED_tractorSmall/dr3/dust/v0_0
-## also need these dirs as tarballs
-## /global/cscratch1/sd/desiproc/dr3
-## /global/cscratch1/sd/desiproc/unwise-coadds
-## /global/cscratch1/sd/ameisner/unwise-coadds/{time_resolved_dr3,fulldepth_sv}
-## Dust map
-## this? /global/homes/d/dstn/tractor/wise/wise-psf-avg.fits
-
-
-## Point it to a specific bashrc??
-## https://github.com/legacysurvey/legacypipe/blob/master/bin/bashrc
 
 ## Equally distributed across nodes
 #module load dws
@@ -46,41 +38,52 @@ ls $DW_PERSISTENT_STRIPED_tractorSmall
 echo ls ${outdir}
 ls ${outdir}
 
-##brick=2501p162
 brick=2523p355
 
+set +x
 module use /project/projectdirs/m888/csdaley/Modules/${NERSC_HOST}/modulefiles
 module unload darshan
 module load ipm-wrap-more-io-kernel-stats/2.0.3-git_serial-io-preload
+##module load ipm-wrap-more-io-kernel-stats-verbose/2.0.3-git_serial-io-preload
+set -x
 
-export do_multi=yes
-if [ "$do_multi" == "yes" ]; then
-    # Multi node test
-    ncores=32
-    for node in {1..64}; do
-        tracdir=${outdir}/multi64/tractor/node${node}
-        ipmdir="${outdir}/multi64/ipm/node${node}"
-        mkdir -p $ipmdir
-        export IPM_LOGDIR=$ipmdir
-        srun -N 1 -n 1 -c ${ncores} --export=ALL,LD_PRELOAD=$IPM_LOAD python legacypipe/runbrick.py --zoom 1 1600 1 1600 --force-all --no-write --pipe --threads ${ncores} --skip --skip-calibs --brick $brick --outdir ${tracdir} --nsigma 6 &
-    done
-    wait
-elif [ "$do_multi" == "no" ]; then
-    tracdir=${outdir}/tractor/ncores${ncores}
-    ipmdir="${outdir}/ipm/ncores${ncores}"
-    rm -r ${tracdir} 
-    rm -r ${ipmdir} 
-    mkdir -p $ipmdir
-    export IPM_LOGDIR=$ipmdir
-    srun -n 1 -c ${ncores} --export=ALL,LD_PRELOAD=$IPM_LOAD python legacypipe/runbrick.py \
-            --zoom 1 1600 1 1600 \
-            --force-all --no-write \
-            --pipe \
-            --threads ${ncores} \
-            --skip \
-            --skip-calibs \
-            --brick $brick --outdir ${tracdir} --nsigma 6
-fi 
+EXE=python ../legacypipe/runbrick.py \
+    --zoom 1 1600 1 1600 \
+    --force-all --no-write \
+    --pipe \
+    --threads ${ncores} \
+    --skip \
+    --skip-calibs \
+    --brick $brick --outdir ${tracdir} --nsigma 6
+
+for process in $(seq 1 ${SLURM_JOB_NUM_NODES}); do
+    echo "Launching process ${process}"
+	mkdir bb_${process}
+	cd bb_${process}
+    srun \
+	-N 1 \
+	-n 1 \
+	-c ${ncores} \
+	-o "output.runbrick.%j.${process}" \
+	-e "error.runbrick.%j.${process}" \
+	$LAUNCH $EXE &
+    cd ..
+done
+sleep 10
+touch $CONTROL_FILE
+echo "STAMP RUNBRICK $(date --rfc-3339=ns)"
+t1=$(date +%s.%N)
+wait
+rm $CONTROL_FILE
+t2=$(date +%s.%N)
+tdiff=$(echo "$t2 - $t1" | bc -l)
+echo "TIME RUNBRICK $tdiff"
+
+# Copy the stdout, stderr and IPM XML file - CD: commented out for now but may be needed when we run on the BB
+#for process in $(seq 1 ${SLURM_JOB_NUM_NODES}); do
+#    cp -n -v {output*,error*,*.xml} ${SOME_FINAL_PFS_DIR}
+#done
+
 #echo RUNNING WITH strace
 #module use /project/projectdirs/m888/csdaley/Modules/${NERSC_HOST}/modulefiles
 #module load strace/4.12
@@ -92,10 +95,6 @@ fi
 #    --skip \
 #    --skip-calibs \
 #    --brick $brick --outdir ${outdir} --nsigma 6
-
-echo ls ${outdir}
-ls ${outdir}
-
 
 #mv ${cori_outdir}/* ${savedir}/
 #if [ "$do_ipm" == "yes" ]; then
--- bb_timing/submit_LUSTER.sh
+++ bb_timing/submit_LUSTER.sh
@@ -1,55 +1,92 @@
 #!/bin/bash -l
 
 #SBATCH -p debug
-#SBATCH -N 64
-#SBATCH -t 00:30:00
+#SBATCH -N 2
+#SBATCH -t 00:05:00
 #SBATCH -J lstr_multi
-#SBATCH -o lstr_multi.o%j
+#SBATCH -o output.%j
+#SBATCH -e error.%j
+#SBATCH -d singleton
 #SBATCH --mail-user=kburleigh@lbl.gov
 #SBATCH --mail-type=END,FAIL
-#SBATCH -L SCRATCH
 
-ncores="$1"
+set -x
+LAUNCH="./sync_launch.sh"
+export CONTROL_FILE="$SCRATCH/control_file_${SLURM_JOBID}.txt"
 
-module use /project/projectdirs/m888/csdaley/Modules/${NERSC_HOST}/modulefiles
-module unload darshan
-module load ipm-wrap-more-io-kernel-stats/2.0.3-git_serial-io-preload
+ncores=32
 
 export LEGACY_SURVEY_DIR=/global/cscratch1/sd/kaylanb/dr3_testdir_for_bb
 export DUST_DIR=${LEGACY_SURVEY_DIR}/dust/v0_0
-#brick=2501p162
+
+## Equally distributed across nodes
+#module load dws
+#echo dwstat --all
+#dwstat --all
+
 brick=2523p355
 
-export do_multi=yes
-if [ "$do_multi" == "yes" ]; then
-    # Multi node test
-    ncores=32
-    for node in {1..64}; do
-        outdir=${SLURM_SUBMIT_DIR}/lstr_multi/multi64/tractor/node${node}
-        ipmdir="${SLURM_SUBMIT_DIR}/lstr_multi/multi64/ipms/node${node}"
-        mkdir -p $ipmdir
-        export IPM_LOGDIR=$ipmdir
-        srun -N 1 -n 1 -c ${ncores} --export=ALL,LD_PRELOAD=$IPM_LOAD python legacypipe/runbrick.py --zoom 1 1600 1 1600 --force-all --no-write --pipe --threads ${ncores} --skip --skip-calibs --brick $brick --outdir ${outdir} --nsigma 6 &
-    done
-    #node=2
-    #outdir=${SLURM_SUBMIT_DIR}/lstr_multi/tractor/node${node}
-    #ipmdir="${SLURM_SUBMIT_DIR}/lstr_multi/ipms/node${node}"
-    #mkdir -p $ipmdir
-    #export IPM_LOGDIR=$ipmdir
-    #srun -N 1 -n 1 -c ${ncores} --export=ALL,LD_PRELOAD=$IPM_LOAD python legacypipe/runbrick.py --zoom 1 1600 1 1600 --force-all --no-write --pipe --threads ${ncores} --skip --skip-calibs --brick $brick --outdir ${outdir} --nsigma 6 &
-    wait
-elif [ "$do_multi" == "no" ]; then
-    outdir=${SLURM_SUBMIT_DIR}/lstr_timing/wipm/ncores${ncores}
-    ipmdir="${SLURM_SUBMIT_DIR}/lstr_timing/wipm/ipms/ncores${ncores}"
-    mkdir -p $ipmdir
-    export IPM_LOGDIR=$ipmdir
-    srun -n 1 -c ${ncores} --export=ALL,LD_PRELOAD=$IPM_LOAD python legacypipe/runbrick.py \
-            --zoom 1 1600 1 1600 \
-            --force-all --no-write \
-            --pipe \
-            --threads ${ncores} \
-            --skip \
-            --skip-calibs \
-            --brick $brick --outdir ${outdir} --nsigma 6
-fi
+set +x
+module use /project/projectdirs/m888/csdaley/Modules/${NERSC_HOST}/modulefiles
+module unload darshan
+module load ipm-wrap-more-io-kernel-stats/2.0.3-git_serial-io-preload
+##module load ipm-wrap-more-io-kernel-stats-verbose/2.0.3-git_serial-io-preload
+set -x
+
+EXE=python ../legacypipe/runbrick.py \
+    --zoom 1 1600 1 1600 \
+    --force-all --no-write \
+    --pipe \
+    --threads ${ncores} \
+    --skip \
+    --skip-calibs \
+    --brick $brick --outdir ${tracdir} --nsigma 6
+
+for process in $(seq 1 ${SLURM_JOB_NUM_NODES}); do
+    echo "Launching process ${process}"
+	mkdir lstr_${process}
+	cd lstr_${process}
+    srun \
+	-N 1 \
+	-n 1 \
+	-c ${ncores} \
+	-o "output.runbrick.%j.${process}" \
+	-e "error.runbrick.%j.${process}" \
+	$LAUNCH $EXE &
+    cd ..
+done
+sleep 10
+touch $CONTROL_FILE
+echo "STAMP RUNBRICK $(date --rfc-3339=ns)"
+t1=$(date +%s.%N)
+wait
+rm $CONTROL_FILE
+t2=$(date +%s.%N)
+tdiff=$(echo "$t2 - $t1" | bc -l)
+echo "TIME RUNBRICK $tdiff"
+
+# Copy the stdout, stderr and IPM XML file - CD: commented out for now but may be needed when we run on the BB
+#for process in $(seq 1 ${SLURM_JOB_NUM_NODES}); do
+#    cp -n -v {output*,error*,*.xml} ${SOME_FINAL_PFS_DIR}
+#done
+
+#echo RUNNING WITH strace
+#module use /project/projectdirs/m888/csdaley/Modules/${NERSC_HOST}/modulefiles
+#module load strace/4.12
+#srun -n 1 -c ${ncores} $STRACE_LOG python legacypipe/runbrick.py \
+#    --zoom 1 1600 1 1600 \
+#    --force-all --no-write \
+#    --pipe \
+#    --threads ${ncores} \
+#    --skip \
+#    --skip-calibs \
+#    --brick $brick --outdir ${outdir} --nsigma 6
+
+#mv ${cori_outdir}/* ${savedir}/
+#if [ "$do_ipm" == "yes" ]; then
+#    mv ./kaylanb*.xml ${savedir}/
+#elif [ "$do_strace" == "yes" ]; then
+#    mv ./{strace_time.out,strace.out} ${savedir}/
+#fi
+
 echo DONE
