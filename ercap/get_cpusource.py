da9de0ffce7f23f22c9ccd4eae31c1bed4c68308
Kaylan Burleigh <kburleigh@lbl.gov>
2016-09-15 08:32:11 -0700

    gather cpu time from all outputs and extract brick names for each

--- ercap/get_cpusource.py
+++ ercap/get_cpusource.py
@@ -1,10 +1,11 @@
-from mpi4py import MPI
 from astropy.io import fits
 from astropy.table import vstack, Table, Column
 from argparse import ArgumentParser
 import numpy as np
 import os
 import sys
+import glob
+import pickle
 #from functools import partial
 #import traceback
 #from subprocess import check_output
@@ -20,7 +21,12 @@ def rem_if_exists(name):
     if os.path.exists(name):
         if os.system(' '.join(['rm','%s' % name]) ): raise ValueError
 
-def main(args):
+def read_150k_cats(args=None):
+    from mpi4py import MPI
+    parser = ArgumentParser(description="test")
+    parser.add_argument("--cats",action="store",help='text file listing absolute paths to Tractor Catalogues to read CPU_SOURCE from',required=True)
+    args = parser.parse_args(args=args)
+    
     comm = MPI.COMM_WORLD
     rank= comm.rank
     nodes= comm.size
@@ -43,9 +49,57 @@ def main(args):
         i=comm.rank+cnt*comm.size
     print "rank %d finished" % rank
 
-if __name__ == '__main__':
+def gather_results(args=None):
     parser = ArgumentParser(description="test")
-    parser.add_argument("--cats",action="store",help='text file listing absolute paths to Tractor Catalogues to read CPU_SOURCE from',required=True)
-    args = parser.parse_args()
+    parser.add_argument("--search",action="store",default='rank*.txt',help='wildcard string to search for',required=False)
+    parser.add_argument("--savefn",action="store",default='results.pickle',required=False)
+    args = parser.parse_args(args=args)
+    
+    fns=glob.glob(args.search)
+    if len(fns) < 1: 
+        print "fns=",fns
+        raise ValueErrror
+    for cnt,fn in enumerate(fns):
+        if cnt == 0:
+            allcats,alltime= np.loadtxt(fn, dtype=str, delimiter=' ',unpack=True)
+            alltime=alltime.astype(float)
+        else:
+            cats,time= np.loadtxt(fn, dtype=str, delimiter=' ',unpack=True)
+            time=time.astype(float)
+            allcats=np.concatenate((cats,allcats), axis=0)
+            alltime=np.concatenate((time,alltime), axis=0)
+    print 'finished gather'
+    # extract brick names
+    bricks=np.zeros(allcats.size).astype(str)
+    for i in range(allcats.size):
+        bricks[i]= allcats[i][allcats[i].find('tractor-')+8:-5]
+    print 'finished post processing'
+    if os.path.exists(args.savefn): 
+        os.remove(args.savefn)
+    fobj=open(args.savefn,'w')
+    pickle.dump((allcats,bricks,alltime),fobj)
+    fobj.close()    
+    print 'saved results to %s' % args.savefn
 
-    main(args)
+def analyze_results(args=None):
+    parser = ArgumentParser(description="test")
+    parser.add_argument("--savefn",action="store",default='results.pickle',required=False)
+    args = parser.parse_args(args=args)
+
+    fobj=open(args.savefn,'r')
+    f,b,t=pickle.load(fobj)
+    fobj.close()
+
+    print t.sum()/3600
+
+def main(args=None,program='analyze'):
+    if program == 'analyze':
+        analyze_results()
+    elif program == 'gather':
+        gather_results()
+    else: 
+        read_150k_cats()
+
+if __name__ == '__main__':
+    main()
+
