b25e87f682db5a4d602ddbd055d800f35a7b109c
Kaylan Burleigh <kburleigh@lbl.gov>
2016-09-19 06:31:55 -0700

    last update before decided on 2017 ercap numbers

--- ercap/get_cpusource.py
+++ ercap/get_cpusource.py
@@ -88,41 +88,45 @@ def add_scatter(ax,x,y,c='b',m='o',lab='hello',s=80,drawln=False):
     ax.scatter(x,y, s=s, lw=2.,facecolors='none',edgecolors=c, marker=m,label=lab)
     if drawln: ax.plot(x,y, c=c,ls='-')
 
-def plot_medt_grz(cpu,append=''):
+def plot_medt_grz(cpu,append='',max_exp=9):
     name='medt_grz_%s.png' % append 
     fig,ax=plt.subplots()
-    add_scatter(ax,np.arange(cpu.size), cpu/3600., c='b',m='o',lab='',drawln=True)
+    add_scatter(ax,np.arange(cpu.size), cpu, c='b',m='o',lab='',drawln=True)
     plt.legend(loc='lower right',scatterpoints=1)
     #ax.set_yscale('log')
     #ax.set_ylim([1e-3,1e2])
-    xlab=ax.set_ylabel('Median of Brick-summed "cpu_source" [hours]')
-    ylab=ax.set_xlabel('nexp g+r+z (g,r,z <= 5)')
+    xlab=ax.set_ylabel('MPP hours (Median over Bricks)')
+    ylab=ax.set_xlabel('nexp g+r+z (g,r,z <= %d)' % max_exp)
     plt.savefig(name, bbox_extra_artists=[xlab,ylab], bbox_inches='tight',dpi=150)
     plt.close() 
  
-def plot_medt_grz_errbars(cpu):
+def plot_medt_grz_errbars(cpu,cdf,max_exp=9):
     name='medt_grz_errbars.png'
-    yerr=np.vstack((cpu['med']-cpu['min'],cpu['max']-cpu['med']))/3600. 
-    plt.errorbar(range(cpu['med'].size), cpu['med']/3600.,yerr=yerr,\
+    yerr=np.vstack((cpu['med']-cpu['min'],cpu['max']-cpu['med'])) 
+    plt.errorbar(range(cpu['med'].size), cpu['med'],yerr=yerr,\
                  marker='o',fmt='o',c='b')
     # nexp= 3,6,9 are special
-    i=[3,6,9]
-    yerr=yerr[:,i] 
-    plt.errorbar(np.arange(cpu['med'].size)[i], cpu['med'][i]/3600.,yerr=yerr,\
+    imp=np.arange(max_exp*3)[::3][1:]
+    yerr=yerr[:,imp] 
+    plt.errorbar(np.arange(cpu['med'].size)[imp], cpu['med'][imp],yerr=yerr,\
                  marker='o',fmt='o',c='r')
-    for i in [3,6,9]:
-        plt.text(i,cpu['med'][i]/3600.,'%.2f' % (cpu['med'][i]/3600.,),ha='left',va='center')
-        plt.text(i,cpu['max'][i]/3600.,'%.2f' % (cpu['max'][i]/3600.,),ha='left',va='center')
-        plt.text(i,cpu['min'][i]/3600.,'%.2f' % (cpu['min'][i]/3600.,),ha='left',va='center')
+    for i in imp:
+        plt.text(i,cpu['med'][i],'%.2f' % (cpu['med'][i],),ha='left',va='top',fontsize='small')
+        plt.text(i,cpu['max'][i],'%.2f' % (cpu['max'][i],),ha='left',va='top',fontsize='small')
+        plt.text(i,cpu['min'][i],'%.2f' % (cpu['min'][i],),ha='left',va='top',fontsize='small')
+    # cumulative %
+    for i in range(max_exp*3):
+        plt.text(i,cpu['max'][i],'%d%%' % (cdf[i],),ha='center',va='bottom',fontsize='small')
     #plt.legend(loc='lower right',scatterpoints=1)
     #ax.set_yscale('log')
     #ax.set_ylim([1e-3,1e2])
-    xlab=plt.ylabel('Median of Brick-summed "cpu_source" [hours]')
-    ylab=plt.xlabel('nexp g+r+z (g,r,z <= 5)')
+    xlab=plt.ylabel('MPP hours (Median over Bricks)')
+    ylab=plt.xlabel('nexp g+r+z (g,r,z <= %d)' % max_exp)
     plt.savefig(name, bbox_extra_artists=[xlab,ylab], bbox_inches='tight',dpi=150)
     plt.close() 
  
-def analyze_results(args=None):
+def analyze_results(args=None,max_exp=6):
+    '''max_band_exp = 9 b/c at most have 3 exposures overlapping same time, 3 passes each'''
     parser = ArgumentParser(description="test")
     parser.add_argument("--savefn",action="store",default='results.pickle',required=False)
     args = parser.parse_args(args=args)
@@ -142,23 +146,37 @@ def analyze_results(args=None):
     # indices
     b={}
     for band in ['g','r','z']:
-        b['%s5' % band]=info['nexp_%s' % band] <= 5
+        b['%s' % band]=info['nexp_%s' % band] <= max_exp
     grz= info['nexp_g']+info['nexp_r']+info['nexp_z']
-    for i in range(16):
+    for i in range(max_exp*3):
         b['grz%d' % i]= grz == i
     # data
+    cdf=np.zeros(max_exp*3).astype(float)
     cpu={}
-    for nam in ['min','max','med']: cpu[nam]= np.zeros(16)-1
-    for i in range(16):
-        cut= b['g5']*b['r5']*b['z5']*b['grz%d' % i]
+    for nam in ['min','max','med']: cpu[nam]= np.zeros(max_exp*3)-1
+    for i in range(max_exp*3):
+        cut= b['g']*b['r']*b['z']*b['grz%d' % i]
         cpu['med'][i]= np.median( times[cut] )
         cpu['min'][i]= np.min( times[cut] )
         cpu['max'][i]= np.max( times[cut] )
+        cdf[i]= times[cut].size
+    cdf=np.cumsum(cdf)
+    cdf= cdf/bricks.size*100
+    cdf.astype(int)
+    # Convert to MPP hours, this is what nersc charges
+    # MPP hours = wall[hrs] * nodes * cores * machine factor (2/2.5 edison/cori) * queue factor
+    for nam in ['min','max','med']: 
+        cpu[nam]= cpu[nam]/6./3600. * 1. * 6. * 2.5 * 2. #sum(cpu_source in cat)/6 is ~ wall time
     # plot
-    for nam in ['min','max','med']: plot_medt_grz(cpu[nam],append=nam)
-    plot_medt_grz_errbars(cpu)
- 
-    print times.sum()/3600
+    for nam in ['min','max','med']: plot_medt_grz(cpu[nam],append=nam,max_exp=max_exp)
+    plot_medt_grz_errbars(cpu,cdf,max_exp=max_exp)
+    # Print MPP hours 
+    print "total MPP hours dr3 'fitblobs'= %.2f" % (times.sum()/6./3600. * 1. * 6. * 2.5 * 2.,)
+    for passes in [1,2,3]:
+        b=np.ones(info['nexp_g'].size).astype(bool)
+        for band in ['g','r','z']: b*= (info['nexp_%s' % band] >= passes)
+        print "fraction of bricks with median g,r,z >= %d is %.2f" % \
+                (passes,float(info['brickname'][b].size)/info['brickname'].size)
 
 def main(args=None,program='analyze'):
     if program == 'analyze':
