793b1b54684d6b92e350feb2629f60cab839320b
Kaylan Burleigh <kburleigh@lbl.gov>
2016-10-20 11:16:11 -0700

    now using my mongodb at nersc

--- fireworks/add_fireworks.py
+++ fireworks/add_fireworks.py
@@ -11,35 +11,37 @@ def bash(cmd):
         sys.exit() 
 
 parser = ArgumentParser(description="test")
-parser.add_argument("--ncores",type=int,action="store",default='32',required=True)
-parser.add_argument("--brick",action="store",default='2523p355',required=True)
-parser.add_argument("--zoom",type=int,action="store",default='1600',required=True)
+parser.add_argument("--file_list",action="store",required=True)
 parser.add_argument("--outdir",action="store",required=True)
-parser.add_argument("--rundir",action="store",help='absolute path to legacypie/py',required=True)
+parser.add_argument("--script",action="store",help='absolute path to script.py',required=True)
+parser.add_argument("--cores",type=int,action="store",default='32',required=False)
+parser.add_argument("--brick",action="store",default='2523p355',required=False)
+parser.add_argument("--zoom",type=int,action="store",default='1600',required=False)
 args = parser.parse_args()
 
-launchpad= LaunchPad(host="mongodb03",name="db_name",username="db_name_admin",password="db_pass")
+launchpad= LaunchPad(host="mongodb01",name="tractor_fireworks",username="tractor_fireworks_admin",password="w2e23sddf21")
 launchpad.reset('', require_password=False)
 
-ntasks=10
-os.chdir(os.rundir)
-for i in range(ntasks):
-    outdir=os.path.join(args.outdir,"b%s_zm%d_task%d" % (args.brick,args.zoom,i))
-    #os.removedirs(outdir); os.makedirs(outdir); os.chdir(outdir); bash('ln -s %s legacypipe' % args.rundir)
-    name="task-"+str(i)
-    script="python legacypipe/runbrick.py \
-                --zoom 1 %d 1 %d \
-                --force-all --no-write \
-                --pipe \
-                --threads %d \
-                --skip \
-                --skip-calibs \
-                --brick %s --outdir %s. --nsigma 6" % \
-            (args.zooom,args.zoom,args.ncores,args.brick,\
-             outdir)
-
-    print script
-
-    firetask= ScriptTask.from_str(script)
-    firework= Firework(firetask,name=name)
+fns= read_lines(args.file_list)
+for fn in fns:
+    cmd="python %s --images %s" % (args.script,fn)
+    firetask= ScriptTask.from_str(cmd)
+    firework= Firework(firetask, name=os.path.basename(fn))
     launchpad.add_wf(firework)
+print 'added %d fireworks' % (len(fns))
+#os.chdir(os.rundir)
+#for i in range(ntasks):
+#    outdir=os.path.join(args.outdir,"b%s_zm%d_task%d" % (args.brick,args.zoom,i))
+#    #os.removedirs(outdir); os.makedirs(outdir); os.chdir(outdir); bash('ln -s %s legacypipe' % args.rundir)
+#    name="task-"+str(i)
+#    script="python legacypipe/runbrick.py \
+#                --zoom 1 %d 1 %d \
+#                --force-all --no-write \
+#                --pipe \
+#                --threads %d \
+#                --skip \
+#                --skip-calibs \
+#                --brick %s --outdir %s. --nsigma 6" % \
+#            (args.zooom,args.zoom,args.ncores,args.brick,\
+#             outdir)
+
---
+++ fireworks/submit_job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+
+#SBATCH -p debug
+#SBATCH -N 1
+#SBATCH -t 00:05:00
+#SBATCH -J job
+#SBATCH -L SCRATCH
+
+module load python/2.7-anaconda
+export OMP_NUM_THREADS=1
+srun -n 1 python sleep_on_it.py
+
+
